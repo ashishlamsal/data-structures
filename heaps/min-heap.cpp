109df4f465be5d5396c9568c091bc64c723b2d2a
ashish <ashishlamsal4283@gmail.com>
2020-11-02 23:55:40 +0545

    min-heap added

--- heaps/min-heap.cpp
+++ heaps/min-heap.cpp
@@ -3,7 +3,7 @@
 const int MAXSIZE = 10;
 
 template <class T>
-class MaxHeap {
+class MinHeap {
 private:
     T* items;
     int size;
@@ -13,7 +13,7 @@ private:
     int parent(int);
     int leftChild(int);
     int rightChild(int);
-    int largerChild(int);
+    int smallerChild(int);
 
     bool isValidParent(int);
     bool hasLeftChild(int);
@@ -24,66 +24,66 @@ private:
     void swap(int, int); // @param indexes
 
 public:
-    MaxHeap();
-    ~MaxHeap();
+    MinHeap();
+    ~MinHeap();
     
     bool isEmpty();
     bool isFull();
-    T max();
+    
+    T min();
     void insert(const T&);
     T remove();      // removes root node (default)
 
-    static int getKthLargest(int* arr, int, int);
     void display();
 };
 
 template <class T>
-MaxHeap<T>::MaxHeap() {
+MinHeap<T>::MinHeap() {
     items = new T[MAXSIZE];
     size = 0;
 }
 
 template <class T>
-MaxHeap<T>::~MaxHeap() {
+MinHeap<T>::~MinHeap() {
     delete[] items;
 }
 
 template <class T>
-bool MaxHeap<T>::isEmpty() {
+bool MinHeap<T>::isEmpty() {
     return size == 0;
 }
 
 template <class T>
-bool MaxHeap<T>::isFull() {
+bool MinHeap<T>::isFull() {
     return size == MAXSIZE;
 }
 
 template <class T>
-int MaxHeap<T>::parent(int index) {
+int MinHeap<T>::parent(int index) {
     return (index - 1) / 2;
 }
 
 template <class T>
-int MaxHeap<T>::leftChild(int index) {
+int MinHeap<T>::leftChild(int index) {
     return (index * 2) + 1;
 }
 
 template <class T>
-int MaxHeap<T>::rightChild(int index) {
+int MinHeap<T>::rightChild(int index) {
     return (index * 2) + 2;
 }
 
 template <class T>
-void MaxHeap<T>::swap(int first, int second) {
+void MinHeap<T>::swap(int first, int second) {
     T temp = items[first];
     items[first] = items[second];
     items[second] = temp;
 }
 
 template <class T>
-T MaxHeap<T>::max() {
+T MinHeap<T>::min() {
     if (isEmpty()) {
-        std::cout << "ERROR: MaxHeap Underflow" << std::endl;
+        std::cout << "ERROR: MinHeap Underflow" << std::endl;
         exit(1);
     }
 
@@ -91,9 +91,9 @@ T MaxHeap<T>::max() {
 }
 
 template <class T>
-void MaxHeap<T>::insert(const T& value) {
+void MinHeap<T>::insert(const T& value) {
     if (isFull()) {
-        std::cout << "ERROR: MaxHeap Overflow" << std::endl;
+        std::cout << "ERROR: MinHeap Overflow" << std::endl;
         return;
     }
     items[size++] = value;
@@ -102,18 +102,18 @@ void MaxHeap<T>::insert(const T& value) {
 }
 
 template <class T>
-void MaxHeap<T>::bubbleUp() {
+void MinHeap<T>::bubbleUp() {
     int index = size - 1;
-    while (index > 0 && items[index] > items[parent(index)]) {
+    while (index > 0 && items[index] < items[parent(index)]) {
         swap(index, parent(index));
         index = parent(index);
     }
 }
 
 template <class T>
-T MaxHeap<T>::remove() {
+T MinHeap<T>::remove() {
     if (isEmpty()) {
-        std::cout << "ERROR: MaxHeap Underflow" << std::endl;
+        std::cout << "ERROR: MinHeap Underflow" << std::endl;
         exit(1);
     }
 
@@ -127,68 +127,51 @@ T MaxHeap<T>::remove() {
 }
 
 template <class T>
-void MaxHeap<T>::bubbleDown() {
+void MinHeap<T>::bubbleDown() {
     int index = 0;
     while (index <= size && !isValidParent(index)) {
-        int largerIndex = largerChild(index);
-        swap(index, largerIndex);
-        index = largerIndex;
+        int smallerIndex = smallerChild(index);
+        swap(index, smallerIndex);
+        index = smallerIndex;
     }
 }
 
 // any index must be < size (<= because size is already decreased in remove() method)
 template <class T>
-bool MaxHeap<T>::hasLeftChild(int index) {
+bool MinHeap<T>::hasLeftChild(int index) {
     return leftChild(index) <= size;
 }
 
 template <class T>
-bool MaxHeap<T>::hasRightChild(int index) {
+bool MinHeap<T>::hasRightChild(int index) {
     return rightChild(index) <= size;
 }
 
 template <class T>
-bool MaxHeap<T>::isValidParent(int index) {
+bool MinHeap<T>::isValidParent(int index) {
     if (!hasLeftChild(index))
         return true;
     if (!hasRightChild(index))
-        return items[index] >= items[leftChild(index)];
+        return items[index] <= items[leftChild(index)];
 
-    return items[index] >= items[leftChild(index)] && items[index] >= items[rightChild(index)];
+    return items[index] <= items[leftChild(index)] && items[index] <= items[rightChild(index)];
 }
 
 template <class T>
-int MaxHeap<T>::largerChild(int index) {
+int MinHeap<T>::smallerChild(int index) {
     if (!hasLeftChild(index))
         return index;
     if (!hasRightChild(index))
         return leftChild(index);
 
-    return (items[leftChild(index)] > items[rightChild(index)]) ?
+    return (items[leftChild(index)] < items[rightChild(index)]) ?
         leftChild(index) : rightChild(index);
 }
 
 template <class T>
-int MaxHeap<T>::getKthLargest(int arr[], int n, int k) {
-    if (k < 1 || k > n) {
-        std::cout << "ERROR: Invalid Range" << std::endl;
-        exit(1);
-    }
-
-    MaxHeap<int> h;
-    for (int i = 0; i < n; i++)
-        h.insert(arr[i]);
-
-    for (int i = 0; i < k - 1; i++)
-        h.remove();
-
-    return h.max();
-}
-
-template <class T>
-void MaxHeap<T>::display()
+void MinHeap<T>::display()
 {
-    std::cout << "Max Heap : [  ";
+    std::cout << "Min Heap : [  ";
     for (int i = 0; i < size; i++)
     {
         std::cout << items[i] << ", ";
@@ -201,7 +184,7 @@ template <class T>
 class PriorityQueue
 {
 private:
-    MaxHeap<T> heap;
+    MinHeap<T> heap;
 public:
     void enqueue(const T&);
     T dequeue();
@@ -231,7 +214,7 @@ void PriorityQueue<T>::display() {
 }
 
 int main() {
-    MaxHeap<int> heap;
+    MinHeap<int> heap;
     heap.insert(5);
     heap.insert(10);
     heap.insert(17);
@@ -241,39 +224,19 @@ int main() {
     heap.insert(15);
     heap.display();
     std::cout << heap.remove() << " is removed" << std::endl;
+    std::cout << heap.remove() << " is removed" << std::endl;
     heap.display();
     std::cout << heap.remove() << " is removed" << std::endl;
     heap.display();
 
-
-    // heap sort
-    MaxHeap<int> h1;
-    int arr[] = { 10, 4,3, 50, 40, 5,8 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    for (auto&& item : arr)
-        h1.insert(item);
-
-    for (int i = n - 1; i >= 0; i--) // ascending order | [0 to n (descending)]
-        arr[i] = h1.remove();
-
-    std::cout << "\nSorted array : [  ";
-    for (int i = 0; i < n; i++)
-        std::cout << arr[i] << ", ";
-    std::cout << "\b\b  ]\n";
-
-    int k = 3;
-    std::cout << "Kth largest element (k=" << k << ") : "
-        << MaxHeap<int>::getKthLargest(arr, n, k) << "\n\n";
-
-    // priority queue
-    PriorityQueue<float> queue;
-    queue.enqueue(30.5);
-    queue.enqueue(20.2);
-    queue.enqueue(40.6);
-    queue.enqueue(10.6);
-    queue.display();
-    std::cout << queue.dequeue() << " is removed" << std::endl;
-    queue.display();
+    // priority pqueue
+    PriorityQueue<float> pqueue;
+    pqueue.enqueue(30.5);
+    pqueue.enqueue(20.2);
+    pqueue.enqueue(40.6);
+    pqueue.enqueue(10.6);
+    pqueue.display();
+    std::cout << pqueue.dequeue() << " is removed" << std::endl;
+    pqueue.display();
     return 0;
 }
